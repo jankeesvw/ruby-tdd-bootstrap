0b7e5c3a8bb6b4c7d1293ce4492c81853745980d
Jankees van Woezik <jankees@base42.nl>
2014-09-06 07:36:09 +0200

    Fix some :fish:

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,13 +1,9 @@
 require "spec_helper"
 
 describe App do
-
-  before :each do
-    @app = App.new
-  end
+  before(:each) { @app = App.new }
 
   it "should say hello" do
     expect(@app.say).to eq "hello"
   end
-
 end
